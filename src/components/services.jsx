306d327ebf949e96a4350affeccf66e154e6472a
adrleo24 <adrianus@axion.ltd>
2024-12-13 10:52:19 +0700

    fix styling

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -5,11 +5,8 @@ export const Services = (props) => {
     <div id="services" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Our Services</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
+          <h2>Modul Yang Tersedia</h2>
+          <p>Sistem terpadu untuk semua bisnis</p>
         </div>
         <div className="row">
           {props.data
